1ac1475e2be1618bd0df11616f1cd1310baf8271
Alessia Pizzoccheri <alessia.pizzoccheri@secfi.com>
2025-07-29 15:37:59 +0200

    obscure studio hostname

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,10 +2,11 @@ import { defineCliConfig } from "sanity/cli";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const studio = process.env.NEXT_STUDIO_HOST;
 
 export default defineCliConfig({
   api: { projectId, dataset },
-  studioHost: "twentyoneforty",
+  studioHost: studio,
   vite: {
     define: {
       "process.env.NEXT_PUBLIC_SANITY_PROJECT_ID": JSON.stringify(projectId),
