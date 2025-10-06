24bd993ba624a27b87d34f3e5f851971140f4a15
Alessia Pizzoccheri <apizzoccheri@protonmail.com>
2025-10-06 19:00:49 +0200

    fix more imports

--- app/templates/page/page-template.tsx
+++ app/templates/page/page-template.tsx
@@ -1,16 +1,16 @@
-import { PageTitle } from "app/shared/components/page-title/page-title";
-import { SlicesComp } from "app/shared/layouts/slices-comp/slices-comp";
-import { PageQueryResultType } from '@/sanity/lib/results";
+import { PageQueryResultType } from '@/sanity/lib/results'
+import { PageTitle } from 'app/shared/components/page-title/page-title'
+import { SlicesComp } from 'app/shared/layouts/slices-comp/slices-comp'
 
 export const PageTemplate = ({
   title,
   excerpt,
-  slices,
+  slices
 }: PageQueryResultType) => {
   return (
     <>
       <PageTitle title={title} excerpt={excerpt} />
       {slices && <SlicesComp slices={slices} />}
     </>
-  );
-};
+  )
+}
