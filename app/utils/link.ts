68247e0871d3b361527ca183a4114e2978d1b111
Alessia Pizzoccheri <apizzoccheri@protonmail.com>
2025-10-06 19:02:33 +0200

    more fixes

--- app/utils/link.ts
+++ app/utils/link.ts
@@ -1,45 +1,45 @@
-import { ResolvedLink } from "app/types/resolved-link";
-import { InternalResultType, LinkResultType } from '@/sanity/lib/results";
+import { ResolvedLink } from 'app/types/resolved-link'
+import { InternalResultType, LinkResultType } from '@/sanity/lib/results'
 
 export const getInternalLinkUrl = (link: InternalResultType): string => {
   switch (link.document) {
     // case "post":
     //   return `/${link.document}/${link.slug}`;
-    case "page":
+    case 'page':
     default:
-      return `/${link.slug}`;
+      return `/${link.slug}`
   }
-};
+}
 
 export const resolveInternalOrExternalLink = (
   link: LinkResultType
 ): ResolvedLink => {
   let result = {
-    _key: link._key,
-  };
+    _key: link._key
+  }
 
   if (link?.internal) {
     return {
       ...result,
       _type: link.internal._type,
       label: link.internal.label,
-      href: getInternalLinkUrl(link.internal),
-    };
+      href: getInternalLinkUrl(link.internal)
+    }
   }
 
   if (link?.external) {
     return {
       ...result,
       _type: link.external._type,
-      label: link.external?.label || "",
-      href: link.external.url,
-    };
+      label: link.external?.label || '',
+      href: link.external.url
+    }
   }
 
   return {
-    _key: "",
-    _type: "",
-    label: "",
-    href: "",
-  };
-};
+    _key: '',
+    _type: '',
+    label: '',
+    href: ''
+  }
+}
