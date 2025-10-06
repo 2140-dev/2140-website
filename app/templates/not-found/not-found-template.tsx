fe773a86f747c73e7daf252c3cb28b3c333d8372
Alessia Pizzoccheri <apizzoccheri@protonmail.com>
2025-10-06 19:11:35 +0200

    remove sx props

--- app/templates/not-found/not-found-template.tsx
+++ app/templates/not-found/not-found-template.tsx
@@ -1,38 +1,31 @@
 import styles from './not-found-template.module.scss'
-import { PageNotFoundQueryResultType } from '@/sanity/lib/results';
-import { Container } from "app/shared/layouts/container/container";
-import { Eyebrow } from "app/shared/components/eyebrow/eyebrow";
-import { RichTextRenderer } from "app/shared/components/rich-text-renderer/rich-text-renderer";
-import { resolveInternalOrExternalLink } from "app/utils/link";
-import { Button } from "app/shared/components/button/button";
+import { PageNotFoundQueryResultType } from '@/sanity/lib/results'
+import { Container } from 'app/shared/layouts/container/container'
+import { Eyebrow } from 'app/shared/components/eyebrow/eyebrow'
+import { RichTextRenderer } from 'app/shared/components/rich-text-renderer/rich-text-renderer'
+import { resolveInternalOrExternalLink } from 'app/utils/link'
+import { Button } from 'app/shared/components/button/button'
 export const NotFoundTemplate = ({
   eyebrow,
   title,
   content,
-  items,
+  items
 }: PageNotFoundQueryResultType) => {
   return (
     <div className={styles['not-found']}>
       <Container size="sm">
         {eyebrow && <Eyebrow color="blue" text={eyebrow} />}
-        <h1>
-          {title}
-        </h1>
+        <h1>{title}</h1>
         {content && <RichTextRenderer content={content} />}
         {items?.map((item, index) => {
-          const { href, label } = resolveInternalOrExternalLink(item);
+          const { href, label } = resolveInternalOrExternalLink(item)
           return (
-            <Button
-              key={index}
-              variant="secondary"
-              href={href}
-              sx={{ mt: "4rem" }}
-            >
+            <Button key={index} variant="secondary" href={href}>
               {label}
             </Button>
-          );
+          )
         })}
       </Container>
     </div>
-  );
-};
+  )
+}
